8753cd03260cb4c1f50ab26fef1120e36c009328
Isaac Murchie <isaac@saucelabs.com>
2018-07-10 09:32:26 -0400

    Update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,23 @@
-var xcode = require('appium-xcode');
-var appPaths = require('./');
-var Q = require('q');
-var fs = require('fs');
-var renameFile = Q.denodeify(fs.rename);
+const xcode = require('appium-xcode');
+const appPaths = require('./');
+const B = require('bluebird');
+const fs = require('fs');
+const renameFile = B.promisify(fs.rename);
+const rimraf = require('rimraf');
 
-module.exports = function(grunt) {
 
+module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
   });
 
-  var MAX_BUFFER_SIZE = 524288;
-  var spawn = require('child_process').spawn;
-  var exec = require('child_process').exec;
+  const MAX_BUFFER_SIZE = 524288;
+  const spawn = require('child_process').spawn;
+  const exec = require('child_process').exec;
 
-  var cleanApp = function(appRoot, sdk, done) {
-    var cmd = 'xcodebuild -sdk ' + sdk + ' clean';
-    var xcode = exec(cmd, {cwd: appRoot, maxBuffer: MAX_BUFFER_SIZE}, function (err, stdout, stderr) {
+  const cleanApp = function(appRoot, sdk, done) {
+    const cmd = `xcodebuild -sdk ${sdk} clean`;
+    const xcode = exec(cmd, {cwd: appRoot, maxBuffer: MAX_BUFFER_SIZE}, function (err, stdout, stderr) {
       if (err) {
         grunt.log.error("Failed cleaning app");
         grunt.warn(stderr);
@@ -26,21 +27,20 @@ module.exports = function(grunt) {
     });
   }
 
-  var buildApp = function(appRoot, sdk, done) {
-
+  const buildApp = function(appRoot, sdk, done) {
     grunt.log.write("Building app...");
-    var args = ['-sdk', sdk];
+    let args = ['-sdk', sdk];
     if (process.env.XCCONFIG_FILE) {
       args.push('-xcconfig', process.env.XCCONFIG_FILE);
     }
-    xcode = spawn('xcodebuild', args, {
+    const xcode = spawn('xcodebuild', args, {
       cwd: appRoot,
     });
     xcode.on("error", function (err) {
       grunt.warn("Failed spawning xcodebuild: " + err.message);
     });
-    var output = '';
-    var collect = function (data) { output += data; };
+    let output = '';
+    const collect = function (data) { output += data; };
     xcode.stdout.on('data', collect);
     xcode.stderr.on('data', collect);
     xcode.on('exit', function (code) {
@@ -56,26 +56,34 @@ module.exports = function(grunt) {
     });
   };
 
-  var renameAll = function(done) {
-    var renamePromises = [
-      renameFile('build/Release-iphonesimulator/UICatalog.app', 'build/Release-iphonesimulator/UICatalog-iphonesimulator.app'),
-      renameFile('build/Release-iphoneos/UICatalog.app', 'build/Release-iphoneos/UICatalog-iphoneos.app')
+  const renameAll = function (done) {
+    const safeRenameFile = function (file, newFile) {
+      return renameFile(file, newFile).catch(function (err) {
+        if (!err.message.includes('ENOENT')) {
+          throw err;
+        }
+      });
+    }
+    const renamePromises = [
+      safeRenameFile('build/Release-iphonesimulator/UICatalog.app', 'build/Release-iphonesimulator/UICatalog-iphonesimulator.app'),
+      safeRenameFile('build/Release-iphoneos/UICatalog.app', 'build/Release-iphoneos/UICatalog-iphoneos.app')
     ];
 
-    Q.all(
-    renamePromises,
-    function () {
-      grunt.log.write("finished renaming apps");
-      done();
-    },
-    function (e) {
-      grunt.log.error("could not rename apps");
-      done(e);
-    });
+    B.all(
+      renamePromises,
+      function () {
+        grunt.log.write("finished renaming apps");
+        done();
+      },
+      function (e) {
+        grunt.log.error("could not rename apps");
+        done(e);
+      }
+    );
   }
 
-  var getSdks = function () {
-    var sdks = ['iphonesimulator'];
+  const getSdks = function () {
+    let sdks = ['iphonesimulator'];
     if (process.env.IOS_REAL_DEVICE || process.env.REAL_DEVICE) {
       sdks.push('iphoneos');
     }
@@ -83,33 +91,34 @@ module.exports = function(grunt) {
   }
 
 
-  grunt.registerTask('build', 'build ios app', function(sdk) { buildApp('.', sdk, this.async()) } );
-  grunt.registerTask('clean', 'cleaning', function(sdk) { cleanApp('.', sdk, this.async()) } );
-  grunt.registerTask('renameAll', 'renaming apps', function(path){ renameAll(this.async()) } );
+  grunt.registerTask('build', 'build ios app', function (sdk) { buildApp('.', sdk, this.async()) } );
+  grunt.registerTask('clean', 'cleaning', function (sdk) { cleanApp('.', sdk, this.async()) } );
+  grunt.registerTask('renameAll', 'renaming apps', function (path) { renameAll(this.async()) } );
+  grunt.registerTask('deleteBuild', 'deleting build dir', function () { rimraf('./build', this.async()) });
   grunt.registerTask('cleanAll', 'cleaning', function () {
-    var done = this.async();
+    const done = this.async();
     function runTasks (sdkVer) {
       getSdks().forEach(function (sdk) {
         sdk = sdk + sdkVer;
-        grunt.task.run('clean:' + sdk);
+        grunt.task.run(`clean:${sdk}`);
       });
     }
     xcode.getMaxIOSSDK()
-      .then(function(sdkVer) {
+      .then(function (sdkVer) {
         runTasks(sdkVer);
         done();
       }).catch(function (err) {
         // sometimes this fails on the first try for unknown reasons
-        console.log('Error getting max iOS SDK:', err.message);
+        console.log(`Error getting max iOS SDK: ${err.message}`);
         console.log('Trying again...');
         xcode.getMaxIOSSDK()
-          .then(function(sdkVer) {
+          .then(function (sdkVer) {
             runTasks(sdkVer);
             done();
           }).catch(function (err) {
-            console.log('Error getting max iOS SDK:', err.message);
+            console.log(`Error getting max iOS SDK: ${err.message}`);
             if (process.env.PLATFORM_VERSION) {
-              console.log('Using process.env.PLATFORM_VERSION version:', process.env.PLATFORM_VERSION);
+              console.log(`Using process.env.PLATFORM_VERSION version: ${process.env.PLATFORM_VERSION}`);
               let sdkVer = process.env.PLATFORM_VERSION;
               runTasks(sdkVer);
               done();
@@ -118,21 +127,21 @@ module.exports = function(grunt) {
       });
   });
 
-  grunt.registerTask('buildAll', 'building', function() {
-    var done = this.async();
+  grunt.registerTask('buildAll', 'building', function () {
+    const done = this.async();
     function runTasks (sdkVer) {
       getSdks().forEach(function (sdk) {
         sdk = sdk + sdkVer;
-        grunt.task.run('build:' + sdk);
+        grunt.task.run(`build:${sdk}`);
       });
     }
-    xcode.getMaxIOSSDK().then(function(sdkVer) {
+    xcode.getMaxIOSSDK().then(function (sdkVer) {
       runTasks(sdkVer);
       done();
     }).catch(function (err) {
-      console.log('Error getting max iOS SDK:', err.message);
+      console.log(`Error getting max iOS SDK: ${err.message}`);
       if (process.env.PLATFORM_VERSION) {
-        console.log('Using process.env.PLATFORM_VERSION version:', process.env.PLATFORM_VERSION);
+        console.log(`Using process.env.PLATFORM_VERSION version: ${process.env.PLATFORM_VERSION}`);
         let sdkVer = process.env.PLATFORM_VERSION;
         runTasks(sdkVer);
         done();
@@ -140,5 +149,5 @@ module.exports = function(grunt) {
     });
   });
 
-  grunt.registerTask('default', ['cleanAll', 'buildAll', 'renameAll']);
+  grunt.registerTask('default', ['cleanAll', 'deleteBuild', 'buildAll', 'renameAll']);
 }
