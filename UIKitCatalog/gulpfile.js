be5fcd1d7024fd07cd2677a606c1b72f1dcb6a78
Isaac Murchie <isaac.appium@gmail.com>
2019-08-22 13:40:26 -0400

    feat: [breaking change] make both apps available

--- UIKitCatalog/gulpfile.js
+++ UIKitCatalog/gulpfile.js
@@ -2,8 +2,13 @@
 
 const gulp = require('gulp');
 const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
-const { relative } = require('..');
+const path = require('path');
+const xcode = require('appium-xcode');
+const log = require('fancy-log');
+const { uiKitCatalog } = require('..');
 
+
+const appName = 'UIKitCatalog.app';
 
 boilerplate({
   build: 'ios-uicatalog',
@@ -11,9 +16,22 @@ boilerplate({
   transpile: false,
   iosApps: {
     relativeLocations: {
-      iphoneos: relative.iphoneos,
-      iphonesimulator: relative.iphonesimulator,
+      iphoneos: path.resolve('..', uiKitCatalog.relative.iphoneos),
+      iphonesimulator: path.resolve('..', uiKitCatalog.relative.iphonesimulator),
     },
-    appName: 'UIKitCatalog.app',
+    appName,
   },
+});
+
+
+gulp.task('install', async function (done) {
+  const xcodeVersion = await xcode.getVersion(true);
+  if (xcodeVersion.major < 11) {
+    log(`Not building ${appName} on Xcode version ${xcodeVersion.versionString}`);
+    return done();
+  }
+  return gulp.series('ios-apps:install', (seriesDone) => {
+    seriesDone();
+    done();
+  })();
 });
--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,10 @@
 
 const gulp = require('gulp');
 const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+
+
+boilerplate({
+  build: 'ios-uicatalog',
+  projectRoot: __dirname,
+  transpile: false,
+});
