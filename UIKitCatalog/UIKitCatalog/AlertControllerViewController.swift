477f8563d62984d3058d9f0d472e23ce05f1560e
Isaac Murchie <isaac.appium@gmail.com>
2019-09-10 10:32:51 -0400

    fix: make UIKitCatalog closer to UICatalog

--- UIKitCatalog/UIKitCatalog/AlertControllerViewController.swift
+++ UIKitCatalog/UIKitCatalog/AlertControllerViewController.swift
@@ -11,12 +11,12 @@ class AlertControllerViewController: UITableViewController {
     // MARK: - Properties
 
     weak var secureTextAlertAction: UIAlertAction?
-    
+
 	private enum StyleSections: Int {
 		case alertStyleSection = 0
 		case actionStyleSection
 	}
-	
+
 	private enum AlertStyleTest: Int {
 		// Alert style alerts.
 		case showSimpleAlert = 0
@@ -25,20 +25,20 @@ class AlertControllerViewController: UITableViewController {
 		case showTextEntryAlert
 		case showSecureTextEntryAlert
 	}
-	
+
 	private enum ActionSheetStyleTest: Int {
 		// Action sheet style alerts.
 		case showOkayCancelActionSheet = 0
 		case howOtherActionSheet
 	}
-	
+
 	private var textDidChangeObserver: NSObjectProtocol!
 
     // MARK: - UIAlertControllerStyleAlert Style Alerts
 
     /// Show an alert with an "OK" button.
     func showSimpleAlert() {
-        let title = NSLocalizedString("A Short Title is Best", comment: "")
+        let title = NSLocalizedString("A Short Title Is Best", comment: "")
         let message = NSLocalizedString("A message should be a short, complete sentence.", comment: "")
         let cancelButtonTitle = NSLocalizedString("OK", comment: "")
 
@@ -54,25 +54,25 @@ class AlertControllerViewController: UITableViewController {
 
         present(alertController, animated: true, completion: nil)
     }
-    
+
     /// Show an alert with an "OK" and "Cancel" button.
     func showOkayCancelAlert() {
-        let title = NSLocalizedString("A Short Title is Best", comment: "")
+        let title = NSLocalizedString("A Short Title Is Best", comment: "")
         let message = NSLocalizedString("A message should be a short, complete sentence.", comment: "")
         let cancelButtonTitle = NSLocalizedString("Cancel", comment: "")
         let otherButtonTitle = NSLocalizedString("OK", comment: "")
-        
+
         let alertCotroller = UIAlertController(title: title, message: message, preferredStyle: .alert)
 
         // Create the actions.
         let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { _ in
             print("The \"OK/Cancel\" alert's cancel action occurred.")
         }
-        
+
         let otherAction = UIAlertAction(title: otherButtonTitle, style: .default) { _ in
             print("The \"OK/Cancel\" alert's other action occurred.")
         }
-        
+
         // Add the actions.
         alertCotroller.addAction(cancelAction)
         alertCotroller.addAction(otherAction)
@@ -82,44 +82,44 @@ class AlertControllerViewController: UITableViewController {
 
     /// Show an alert with two custom buttons.
     func showOtherAlert() {
-        let title = NSLocalizedString("A Short Title is Best", comment: "")
+        let title = NSLocalizedString("A Short Title Is Best", comment: "")
         let message = NSLocalizedString("A message should be a short, complete sentence.", comment: "")
         let cancelButtonTitle = NSLocalizedString("Cancel", comment: "")
         let otherButtonTitleOne = NSLocalizedString("Choice One", comment: "")
         let otherButtonTitleTwo = NSLocalizedString("Choice Two", comment: "")
-        
+
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
+
         // Create the actions.
         let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { _ in
             print("The \"Other\" alert's cancel action occurred.")
         }
-        
+
         let otherButtonOneAction = UIAlertAction(title: otherButtonTitleOne, style: .default) { _ in
             print("The \"Other\" alert's other button one action occurred.")
         }
-        
+
         let otherButtonTwoAction = UIAlertAction(title: otherButtonTitleTwo, style: .default) { _ in
             print("The \"Other\" alert's other button two action occurred.")
         }
-        
+
         // Add the actions.
         alertController.addAction(cancelAction)
         alertController.addAction(otherButtonOneAction)
         alertController.addAction(otherButtonTwoAction)
-        
+
         present(alertController, animated: true, completion: nil)
     }
 
     /// Show a text entry alert with two custom buttons.
     func showTextEntryAlert() {
-        let title = NSLocalizedString("A Short Title is Best", comment: "")
+        let title = NSLocalizedString("A Short Title Is Best", comment: "")
         let message = NSLocalizedString("A message should be a short, complete sentence.", comment: "")
         let cancelButtonTitle = NSLocalizedString("Cancel", comment: "")
         let otherButtonTitle = NSLocalizedString("OK", comment: "")
-        
+
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
+
         // Add the text field for text entry.
         alertController.addTextField { _ in
             // If you need to customize the text field, you can do so here.
@@ -129,27 +129,27 @@ class AlertControllerViewController: UITableViewController {
         let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { _ in
             print("The \"Text Entry\" alert's cancel action occurred.")
         }
-        
+
         let otherAction = UIAlertAction(title: otherButtonTitle, style: .default) { _ in
             print("The \"Text Entry\" alert's other action occurred.")
         }
-        
+
         // Add the actions.
         alertController.addAction(cancelAction)
         alertController.addAction(otherAction)
-        
+
         present(alertController, animated: true, completion: nil)
     }
-    
+
     /// Show a secure text entry alert with two custom buttons.
     func showSecureTextEntryAlert() {
-        let title = NSLocalizedString("A Short Title is Best", comment: "")
+        let title = NSLocalizedString("A Short Title Is Best", comment: "")
         let message = NSLocalizedString("A message should be a short, complete sentence.", comment: "")
         let cancelButtonTitle = NSLocalizedString("Cancel", comment: "")
         let otherButtonTitle = NSLocalizedString("OK", comment: "")
-        
+
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
+
         // Add the text field for the secure text entry.
         alertController.addTextField { textField in
             /**	Listen for changes to the text field's text so that we can toggle the current
@@ -169,70 +169,70 @@ class AlertControllerViewController: UITableViewController {
 						}
 					}
 			}
-			
+
             textField.isSecureTextEntry = true
         }
-		
+
         // Create the actions.
         let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { _ in
             print("The \"Secure Text Entry\" alert's cancel action occurred.")
         }
-        
+
         let otherAction = UIAlertAction(title: otherButtonTitle, style: .default) { _ in
             print("The \"Secure Text Entry\" alert's other action occurred.")
         }
-        
+
         /** The text field initially has no text in the text field, so we'll disable it for now.
             It will be re-enabled when the first character is typed.
         */
         otherAction.isEnabled = false
-        
+
         /** Hold onto the secure text alert action to toggle the enabled / disabled
             state when the text changed.
         */
         secureTextAlertAction = otherAction
-        
+
         // Add the actions.
         alertController.addAction(cancelAction)
         alertController.addAction(otherAction)
-        
+
         present(alertController, animated: true, completion: nil)
     }
-    
+
     // MARK: - UIAlertControllerStyleActionSheet Style Alerts
-    
+
     // Show a dialog with an "OK" and "Cancel" button.
     func showOkayCancelActionSheet(_ selectedIndexPath: IndexPath) {
         let message = NSLocalizedString("A message should be a short, complete sentence.", comment: "")
 		let cancelButtonTitle = NSLocalizedString("Cancel", comment: "")
         let destructiveButtonTitle = NSLocalizedString("Confirm", comment: "")
-        
+
         let alertController = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
-        
+
         // Create the actions.
         let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { _ in
             print("The \"OK/Cancel\" alert action sheet's cancel action occurred.")
         }
-        
+
         let destructiveAction = UIAlertAction(title: destructiveButtonTitle, style: .default) { _ in
             print("The \"Confirm\" alert action sheet's destructive action occurred.")
         }
-        
+
         // Add the actions.
         alertController.addAction(cancelAction)
         alertController.addAction(destructiveAction)
-        
+
         // Configure the alert controller's popover presentation controller if it has one.
         if let popoverPresentationController = alertController.popoverPresentationController {
           	// Note for popovers the Cancel button is hidden automatically.
-			
+
 			// This method expects a valid cell to display from.
             let selectedCell = tableView.cellForRow(at: selectedIndexPath)!
             popoverPresentationController.sourceRect = selectedCell.frame
             popoverPresentationController.sourceView = view
             popoverPresentationController.permittedArrowDirections = .up
         }
-        
+
         present(alertController, animated: true, completion: nil)
     }
 
@@ -241,9 +241,9 @@ class AlertControllerViewController: UITableViewController {
         let message = NSLocalizedString("A message should be a short, complete sentence.", comment: "")
 		let destructiveButtonTitle = NSLocalizedString("Destructive Choice", comment: "")
         let otherButtonTitle = NSLocalizedString("Safe Choice", comment: "")
-        
+
         let alertController = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
-        
+
         // Create the actions.
         let destructiveAction = UIAlertAction(title: destructiveButtonTitle, style: .destructive) { _ in
             print("The \"Other\" alert action sheet's destructive action occurred.")
@@ -251,22 +251,22 @@ class AlertControllerViewController: UITableViewController {
         let otherAction = UIAlertAction(title: otherButtonTitle, style: .default) { _ in
             print("The \"Other\" alert action sheet's other action occurred.")
         }
-        
+
         // Add the actions.
         alertController.addAction(destructiveAction)
         alertController.addAction(otherAction)
-        
+
         // Configure the alert controller's popover presentation controller if it has one.
         if let popoverPresentationController = alertController.popoverPresentationController {
             // Note for popovers the Cancel button is hidden automatically.
-			
+
 			// This method expects a valid cell to display from.
             let selectedCell = tableView.cellForRow(at: selectedIndexPath)!
             popoverPresentationController.sourceRect = selectedCell.frame
             popoverPresentationController.sourceView = view
             popoverPresentationController.permittedArrowDirections = .up
         }
-        
+
         present(alertController, animated: true, completion: nil)
     }
 
--- UIKitCatalog/UIKitCatalog/ButtonViewController.swift
+++ UIKitCatalog/UIKitCatalog/ButtonViewController.swift
@@ -5,7 +5,7 @@ Abstract:
 A view controller that demonstrates how to use `UIButton`.
  The buttons are created using storyboards, but each of the system buttons can be created in code by
  using the UIButton.init(type buttonType: UIButtonType) initializer.
- 
+
  See the UIButton interface for a comprehensive list of the various UIButtonType values.
 */
 
@@ -21,7 +21,7 @@ class ButtonViewController: UITableViewController {
     @IBOutlet weak var attributedTextButton: UIButton!
     @IBOutlet weak var symbolButton: UIButton!
     @IBOutlet weak var symbolTextButton: UIButton!
-    
+
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
@@ -71,20 +71,20 @@ class ButtonViewController: UITableViewController {
 		imageButton.setImage(imageButtonNormalImage, for: .normal)
 
         // Add an accessibility label to the image.
-        imageButton.accessibilityLabel = NSLocalizedString("X", comment: "")
+        imageButton.accessibilityLabel = NSLocalizedString("X Button", comment: "")
 
         imageButton.addTarget(self, action: #selector(ButtonViewController.buttonClicked(_:)), for: .touchUpInside)
     }
 
     func configureAttributedTextSystemButton() {
         let buttonTitle = NSLocalizedString("Button", comment: "")
-        
+
         // Set the button's title for normal state.
-		let normalTitleAttributes: [NSAttributedString.Key: Any] = [
+		    let normalTitleAttributes: [NSAttributedString.Key: Any] = [
             NSAttributedString.Key.foregroundColor: UIColor.systemBlue,
             NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue
         ]
-        
+
         let normalAttributedTitle = NSAttributedString(string: buttonTitle, attributes: normalTitleAttributes)
         attributedTextButton.setAttributedTitle(normalAttributedTitle, for: .normal)
 
@@ -98,32 +98,32 @@ class ButtonViewController: UITableViewController {
 
         attributedTextButton.addTarget(self, action: #selector(ButtonViewController.buttonClicked(_:)), for: .touchUpInside)
     }
-    
+
     func configureSymbolButton() {
         let buttonImage = UIImage(systemName: "person")
         symbolButton.setImage(buttonImage, for: .normal)
-        
+
         // Add an accessibility label to the image.
         symbolButton.accessibilityLabel = NSLocalizedString("Person", comment: "")
-        
+
         symbolButton.addTarget(self,
                                action: #selector(ButtonViewController.buttonClicked(_:)),
                                for: .touchUpInside)
     }
-    
+
     func configureSymbolTextButton() {
         let buttonImage = UIImage(systemName: "person")
         symbolTextButton.setImage(buttonImage, for: .normal)
-        
+
         symbolTextButton.addTarget(self,
                                    action: #selector(ButtonViewController.buttonClicked(_:)),
                                    for: .touchUpInside)
-        
+
         symbolTextButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
         let config = UIImage.SymbolConfiguration(textStyle: .body, scale: .small)
         symbolTextButton.setPreferredSymbolConfiguration(config, forImageIn: .normal)
     }
-    
+
     // MARK: - Actions
 
     @objc
